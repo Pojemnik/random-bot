beb98591e07372bea990a57befd964c64cc9555c
Pojemnik <andriej194@gmail.com>
2023-04-13 20:27:54 +0200

    Add version plugin

---
+++ plugins/version/version.py
@@ -0,0 +1,23 @@
+import lightbulb
+from __init__ import ENABLED_GUILDS, cfg
+
+plugin = lightbulb.Plugin("version")
+
+VERSION_STRING = 'v2.2.0'
+
+@plugin.command
+@lightbulb.command(
+    name="version",
+    description="Prints version number",
+    guilds=ENABLED_GUILDS
+)
+@lightbulb.implements(lightbulb.SlashCommand)
+async def version(context: lightbulb.Context) -> None:
+    await context.respond(f'Random bot version: {VERSION_STRING}')
+            
+def load(bot: lightbulb.BotApp) -> None:
+    bot.add_plugin(plugin)
+
+
+def unload(bot: lightbulb.BotApp) -> None:
+    bot.remove_plugin(plugin)
